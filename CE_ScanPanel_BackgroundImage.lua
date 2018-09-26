24ec3769b98328164e9f36cd70bdfb8ac63af902
Benjamin Clements <2312577+FreeER@users.noreply.github.com>
2018-09-26 06:37:27 -0500

    img resize

--- CE_ScanPanel_BackgroundImage.lua
+++ CE_ScanPanel_BackgroundImage.lua
@@ -1,6 +1,5 @@
 img = createImage(MainForm.Panel5)
-img.Width = MainForm.Panel5.Width
-img.Height = MainForm.Panel5.Height
+img.Align = alClient
 img.Stretch = true
 -- test with CE logo
 img.Picture.assign(MainForm.Logo.Picture)
