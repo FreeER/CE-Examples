282897d00f1e27ff28b9b45e61f6c627d1670a15
Benjamin Clements <2312577+FreeER@users.noreply.github.com>
2019-04-04 09:11:33 -0500

    use different symbols

--- aobscanregion and aobscanmodule.lua
+++ aobscanregion and aobscanmodule.lua
@@ -1,35 +1,34 @@
 --https://forum.cheatengine.org/viewtopic.php?p=5747559#5747559
 --[[ SETUP ]]-- 
-function AOBScanAA(script) 
-  local success,disableInfo = autoAssemble(script) 
-  if not success then return nil, disableInfo end -- disableInfo is error message on failure 
-  local addr = getAddress('luaAOBScanRegionSymbol') 
-  autoAssemble(script, disableInfo) -- disable script and unregister symbol 
-  return addr, 'success' 
-end 
+function AOBScanAA(script, symbol)
+  local success,disableInfo = autoAssemble(script)
+  if not success then return nil, disableInfo end -- disableInfo is error message on failure
+  local addr = getAddress(symbol)
+  autoAssemble(script, disableInfo) -- disable script and unregister symbol
+  return addr, 'success'
+end
 
-function AOBScanRegion(bytestr, start, stop) 
-  local script = ([[ 
-  [ENABLE] 
-  aobscanregion(luaAOBScanRegionSymbol,%X,%X,%s) 
-  registersymbol(luaAOBScanRegionSymbol) 
-  [DISABLE] 
-  unregistersymbol(luaAOBScanRegionSymbol) 
-  ]]):format(getAddress(start), getAddress(stop), bytestr) 
-  return AOBScanAA(script) 
-end 
+function AOBScanRegion(bytestr, start, stop)
+  local script = ([[
+  [ENABLE]
+  aobscanregion(luaAOBScanRegionSymbol,%X,%X,%s)
+  registersymbol(luaAOBScanRegionSymbol)
+  [DISABLE]
+  unregistersymbol(luaAOBScanRegionSymbol)
+  ]]):format(getAddress(start), getAddress(stop), bytestr)
+  return AOBScanAA(script, 'luaAOBScanRegionSymbol')
+end
 
-function AOBScanModule(bytestr, module) 
-  local script = ([[ 
-  [ENABLE] 
-  aobscanmodule(luaAOBScanRegionSymbol,%s,%s) 
-  registersymbol(luaAOBScanRegionSymbol) 
-  [DISABLE] 
-  unregistersymbol(luaAOBScanRegionSymbol) 
-  ]]):format(module, bytestr) 
-  return AOBScanAA(script) 
-end 
-
+function AOBScanModule(bytestr, module)
+  local script = ([[
+  [ENABLE]
+  aobscanmodule(luaAOBScanModuleSymbol,%s,%s)
+  registersymbol(luaAOBScanModuleSymbol)
+  [DISABLE]
+  unregistersymbol(luaAOBScanModuleSymbol)
+  ]]):format(module, bytestr)
+  return AOBScanAA(script,'luaAOBScanModuleSymbol')
+end
 --[[ TESTING ]]-- 
 
 -- Tutorial step 1 load 
@@ -37,7 +36,7 @@ end
 local addr,msg = AOBScanModule('8B 83 80040000', process) 
 
 if addr then 
-  print('0x%X'):format(addr) 
+  print(('0x%X'):format(addr))
 else 
   print(msg)
 end 
